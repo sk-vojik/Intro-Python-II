72f538de97d20bc14b3d5c023f290fda220ed17b
sk-vojik <scott.vojik@gmail.com>
2019-04-24 13:02:59 -0700

    initial commit

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, description, contains):
+    self.name = name
+    self.description = description
+    self.contains = []
+
+  
+  def add_item(self, item):
+    self.contains.append(item)
